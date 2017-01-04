6c0c1d963e0719c22fc291053b1f8f98d9fdf6c7
Andrew Garland <andgarland2@gmail.com>
2017-01-03 21:35:56 -0800

    trying db stuff

--- mpr/settings.py
+++ mpr/settings.py
@@ -209,3 +209,14 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 '''
+
+#
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
